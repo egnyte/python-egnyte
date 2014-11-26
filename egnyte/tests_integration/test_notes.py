d4619e26264f31fea8d60fc77099d7511cffd4a4
Maciej Szumocki <mszumocki@egnyte.com>
2014-11-26 17:05:45 +0100

    Notes API

---
+++ egnyte/tests_integration/test_notes.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+import datetime
+
+from egnyte import exc
+from egnyte.tests_integration.config import TestCase
+
+
+class TestNotes(TestCase):
+    def test_notes(self):
+        folder = self.root_folder.folder('notes')
+        folder.create(True)
+        
+        f = folder.file('test.txt')
+        f.upload(b'foobar')
+
+        try:
+            note = f.add_note("this is a test message")
+            all = self.client.notes.list()
+            self.assertIn(note, all)
+            self.assertEqual(f, note.get_file())
+            notes = f.get_notes()
+            self.assertEqual(tuple(notes), (note,))
+            note.delete()
+            notes = f.get_notes()
+            self.assertEqual(tuple(notes), ())
+            all = self.client.notes.list()
+            self.assertNotIn(note, all)
+        finally:
+            f.delete()
+
+
+
